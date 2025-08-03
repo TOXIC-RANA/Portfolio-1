dff11dd8ce7c4681c7171be72603ce3b5c0b7d32
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-08-03 18:04:36 +0600

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,6 +7,7 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
+          "Cyber Secuirity Expert",
           "Freelancer",
           "MERN Stack Developer",
           "Open Source Contributor",
