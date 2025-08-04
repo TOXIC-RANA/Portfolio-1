8b901e030ce8001e2995dc4341e731aa069f171e
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-08-04 19:13:59 +0600

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="FB-STATE"
+              description="A powerful multi-platform C3C FBState Downloader that seamlessly supports Kiwi and Mises Browser. Get your FBState easily and securely with just a few clicks!"
+              ghLink="https://drive.google.com/file/d/10-z_qk0sHDQq2b8kTXao5myP1tWKnHmQ/view/"
+              demoLink="https://drive.google.com/file/d/10-z_qk0sHDQq2b8kTXao5myP1tWKnHmQ/view/"
             />
           </Col>
 
