7cf5d10ec0fce7426d742a674df3b077e0994a11
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-08-04 21:55:28 +0600

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/toxic-rana"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/xaico-rana"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
